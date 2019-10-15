8639f46f3bc3583367092d913fd4cd5d859e1420
Christian Søndergaard Weatherley <alandryl@live.dk>
2019-10-15 21:31:40 +0200

    added flying books.

---
+++ Magic Library/Assets/Assets/Scripts/AnimationRandomStart.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationRandomStart : MonoBehaviour
+{
+    Animator ac;
+
+    void Start()
+    {
+
+        ac = GetComponent<Animator>();
+
+        AnimatorStateInfo state = ac.GetCurrentAnimatorStateInfo(0);//could replace 0 by any other animation layer index
+        ac.Play(state.fullPathHash, -1, Random.Range(0f, 1f));
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
